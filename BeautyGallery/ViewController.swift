9c91f611f2e1728351bbc4c0a070d973d4b9b26b
杨大侠 <873233209@qq.com>
2018-11-02 16:23:42 +0800

    Initial Commit

---
+++ BeautyGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeautyGallery
+//
+//  Created by 杨大侠 on 2018/11/2.
+//  Copyright © 2018 杨大侠. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

dad219025505e57d8394114e1747818dc1c7005c
杨大侠 <873233209@qq.com>
2018-11-02 17:37:33 +0800

    第一次修改
    添加图片

--- BeautyGallery/ViewController.swift
+++ BeautyGallery/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BeautyPicker: UIPickerView!
+    
+    let beauties = ["唐嫣", "张碧晨", "赵丽颖", "迪丽热巴", "杨幂", "杨颖", "古力娜扎"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
